0913958a47741da3a73aa0ebbb1da6a2fa6b69dd
nlamenta <161578683+nlamenta@users.noreply.github.com>
2024-10-14 16:21:19 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,36 +1,38 @@
+import string
+
 # zamiana na małe litery i usunięcie niealfanumerycznych znaków
-replacements = (',', '-', '!', '?', '.')
+def przeksztalc_tekst(wpis):
+    konwerter = str.maketrans('', '', string.punctuation)
+    return wpis.translate(konwerter).lower().split()
 
 # liczba dokumentów
-n = int(input())
+n = int(input("Podaj liczbę dokumentów: "))
 
 # dokumenty
-documents = []
+dokumenty = []
+
 for _ in range(n):
-    my_str = input().lower()  # Zamiana na małe litery
-    for r in replacements:
-        my_str = my_str.replace(r, ' ')  # Zamiana znaków na spacje
-    words = my_str.split()  # Podział na słowa
-    documents.append(words)
+    tresc_dokumentow = input("->").strip()
+    dokumenty.append(przeksztalc_tekst(tresc_dokumentow))  # przetwarzamy dokumenty
 
 # liczba zapytań
-m = int(input())
+m = int(input("Podaj liczbę zapytań: "))
 
 # zapytania
-queries = []
+zapytania = []
 for _ in range(m):
     query = input().strip().lower()
-    queries.append(query)
+    zapytania.append(query)
 
 # przetwarzanie zapytań
-for query in queries:
+for zapytanie in zapytania:
     results = []
 
     # przetwarzanie każdego dokumentu
-    for i, document in enumerate(documents):
-        count = document.count(query)
-        if count > 0:
-            results.append((i, count))  
+    for i, document in enumerate(dokumenty):
+        liczba = document.count(zapytanie)  # zliczamy wystąpienia pojedynczego zapytania
+        if liczba > 0:
+            results.append((i, liczba))
 
     # sortowanie: według liczby wystąpień (malejąco), a potem według indeksu dokumentu (rosnąco)
     results.sort(key=lambda x: (-x[1], x[0]))
